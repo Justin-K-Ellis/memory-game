db3abc21cd5be5f0a865c196e78f6d8674ff8131
Justin Klitgaard <jlke45@gmial.com>
2024-05-11 11:15:53 +0900

    Add main logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import Card from "./components/Card";
+import arrayShuffle from "array-shuffle";
 
-function App() {
-  const [count, setCount] = useState(0)
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const App = () => {
+  const [cardList, setCardList] = useState([]);
+
+  async function fetchData() {
+    let id = randomPokeNumber();
+    const response = await fetch(URL + id, { mode: "cors" });
+    const data = await response.json();
+    const newCard = (
+      <Card name={data.species.name} img={data.sprites.front_default} />
+    );
+    setCardList([...cardList, newCard, newCard]);
+  }
+
+  useEffect(() => {
+    cardList.length < 12 && fetchData();
+  }, [cardList]);
+
+  function randomPokeNumber() {
+    return Math.ceil(Math.random() * 151);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="outer-container">
+      <h1>Pokemon Time</h1>
+      <div className="container">{arrayShuffle(cardList)}</div>
+    </div>
+  );
+};
 
-export default App
+export default App;
