891434f16a448035c2cddeefabc3bb3ad8857f3e
Justin Klitgaard <jlke45@gmial.com>
2024-05-17 13:37:34 +0900

    Factor out score board into component

---
+++ src/components/ScoreBoard.jsx
@@ -0,0 +1,13 @@
+const ScoreBoard = (props) => {
+  return (
+    <div className="rounded border-2 border-white border-solid p-2 w-11/12 mx-auto my-2 flex flex-col text-white justify-center items-center">
+      <p className="text-3xl">{props.currentPlayer}'s Turn</p>
+      <div className="flex flex-row justify-between gap-8 text-2xl">
+        <p>Player 1: {props.playerOnePoints} </p>
+        <p>Player 2: {props.playerTwoPoints}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ScoreBoard;
